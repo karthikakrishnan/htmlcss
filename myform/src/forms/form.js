90b3f82ad0ec38416e75902589afa1c3c8ae9ced
Karthika <kartika.krishnan@gmail.com>
2018-10-21 21:58:36 +0530

    initial commit

---
+++ myform/src/forms/form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Forms extends Component {
+    render() {
+        return(
+            <div>
+                <h3>Contact Form</h3>
+            </div>
+        )
+    }
+}
